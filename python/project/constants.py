d3773410c87e8dd9da6e7090db68caef486c7885
geverartsdev <guillaume.everarts@student.uclouvain.be>
2019-07-09 21:13:43 +0200

    Filling repo with project and syllabus basis

---
+++ python/project/constants.py
@@ -0,0 +1,8 @@
+import pygame
+
+# Constantes de jeu
+MAX_TIRS = 10  # nombre maximum de boulets sur l'ecran
+PROBA_ALIEN = 22  # probabilité qu'un alien apparaisse
+NOUVEL_ALIEN = 12   # Rafraichissement de l'ecran entre chaque alien
+ECRAN = pygame.Rect(0, 0, 1300, 700)
+SONS = False  # Mettre a True si on veut activer le sons
---
+++ python/project/main.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python3
+
+import random
+
+from project.utils import *
+from project.constants import *
+from project.components.vaisseau import Vaisseau, Tir
+from project.components.alien import Alien
+from project.components.explosion import Explosion
+from project.components.score import Score
+from project.components.background import Background
+
+# see if we can load more than standard BMP
+if not pygame.image.get_extended():
+    raise SystemExit("Sorry, extended image module required")
+
+
+main_dir = os.path.split(os.path.abspath(__file__))[0]
+
+
+def images():
+    """
+    Cette methode s'occupe de charger et d'assigner les images au elements du jeu qui en ont besoin
+    :return: ∅
+    """
+    Background.image = load_image('background.png')  # On utilise ici la methode load_image qui a ete definie dans
+    Vaisseau.image = load_image('spacecraft.gif')    # le fichier utils.py
+    img = load_image('explosion1.gif')
+    Explosion.images = [img, pygame.transform.flip(img, 1, 1)]
+    Alien.images = load_images('alien1.gif', 'alien2.gif', 'alien3.gif')
+    Tir.image = load_image('shot.gif')
+
+
+def fenetre():
+    """
+    Cette methode regle la fenetre de jeu, on choisit un titre, une icone, et on definit si on veut voir la souris
+    apparaitre ou non
+    :return: ∅
+    """
+    icon = pygame.transform.scale(Alien.images[0], (32, 32))  # On utilise l'image de l'alien et on la redimensionne
+    pygame.display.set_icon(icon)  # On definit une icone
+    pygame.display.set_caption('Pygame Aliens')  # On choisit le titre
+    pygame.mouse.set_visible(0)  # On cache la souris
+
+
+def sons():
+    """
+    Cette methode s'occupe de charger les sons du jeu et lance la musique principale
+    :return: boom_sound, shoot_sound => Les sons d'explosion et de tir
+    """
+    boom_sound = load_sound('boom.wav')  # On utilise ici la methode load_sound definie dans utils.py
+    shoot_sound = load_sound('car_door.wav')
+    if pygame.mixer and SONS:
+        music = os.path.join(main_dir, 'src/sound', 'house_lo.wav')
+        pygame.mixer.music.load(music)
+        pygame.mixer.music.play(-1)
+
+    return boom_sound, shoot_sound
+
+
+def main():
+    """
+    Fonction principale, appellee lors de l'execution du script
+    :return: ∅
+    """
+    # Initialisation de pygame
+    if pygame.get_sdl_version()[0] == 2:
+        pygame.mixer.pre_init(44100, 32, 2, 1024)
+    pygame.init()
+    if pygame.mixer and not pygame.mixer.get_init():
+        print('Warning, no sound')
+        pygame.mixer = None
+
+    # Definit le mode de la fenetre
+    winstyle = 0  # FULLSCREEN
+    bestdepth = pygame.display.mode_ok(ECRAN.size, winstyle, 32)
+    screen = pygame.display.set_mode(ECRAN.size, winstyle, bestdepth)
+
+    # Chargement des images et assignement aux classes
+    images()
+
+    # Reglage de la fenetre
+    fenetre()
+
+    # Chargement des effets sonores
+    boom_sound, shoot_sound = sons()
+
+    # On cree des groupes de jeu (un peu mystique, ce sont des listes qui vont garder une trace des differents elements
+    # qui seront crees et qui les ajoutent automatiquement lors de leur creation)
+    aliens = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    all = pygame.sprite.RenderUpdates()
+    lastalien = pygame.sprite.GroupSingle()
+
+    # On assigne a chaque classe un (ou des) groupe(s) qui lui correspond(ent)
+    Background.containers = all
+    Vaisseau.containers = all
+    Alien.containers = aliens, all, lastalien
+    Tir.containers = shots, all
+    Explosion.containers = all
+    Score.containers = all
+
+    # On definit quelques valeurs de depart
+    alien_suivant = NOUVEL_ALIEN
+    clock = pygame.time.Clock()
+
+    # On initialise les elements du jeu, nous n'avons pas toujours besoin de garder une reference grace a l'utilisation
+    # des containers, qui s'occupent de les recuperer lors de leur creation (plus de details dans les differents
+    # elements)
+    Background(0)                             # On cree deux images de fond d'ecran, qui se suivent, pour qu'au moins
+    Background(Background.image.get_width())  # une se trouve a chaque fois visible par l'utilisateur
+    player = Vaisseau()  # On cree le joueur, on garde une reference pour pouvoir lui appliquer des actions
+    score = Score()  # On cree le score, on garde une reference pour pouvoir le mettre a jour
+    Alien()  # On cree un premier alien
+
+    # Ici le jeu commence vraiment, dans cette boucle est defini toute la mecanique du jeu
+    while player.alive():
+
+        # get input
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                return
+
+        keystate = pygame.key.get_pressed()
+
+        # On efface le contenu de l'ecran
+        all.clear(screen, pygame.Surface(ECRAN.size))
+
+        # On rafraichit tous les elements
+        all.update()
+
+        # On reagit aux actions du joueur
+        dirx = keystate[pygame.K_RIGHT] - keystate[pygame.K_LEFT]
+        diry = keystate[pygame.K_DOWN] - keystate[pygame.K_UP]
+        player.move((dirx, diry))
+        firing = keystate[pygame.K_SPACE]
+        if not player.reloading and firing and len(shots) < MAX_TIRS:
+            Tir(player.gunpos())
+            if SONS:
+                shoot_sound.play()
+        player.reloading = firing
+
+        # Creation (eventuelle) d'un nouvel alien
+        if alien_suivant > 0:
+            alien_suivant = alien_suivant - 1
+        elif not int(random.random() * PROBA_ALIEN):
+            Alien()
+            alien_suivant = NOUVEL_ALIEN
+
+        # Detection des collisions
+        for alien in pygame.sprite.spritecollide(player, aliens, 1):
+            if SONS:
+                boom_sound.play()
+            Explosion(alien)
+            Explosion(player)
+            score.point()
+            player.kill()
+
+        for alien in pygame.sprite.groupcollide(shots, aliens, 1, 1).keys():
+            if SONS:
+                boom_sound.play()
+            Explosion(alien)
+            score.point()
+
+        # On dessine les elements
+        dirty = all.draw(screen)
+        pygame.display.update(dirty)
+
+        # On definit le teu de rafraichissement de la fenetre
+        clock.tick(40)  # La fenetre ne se rafraichira jamais plus de 40 fois par seconde
+
+    # La partie est terminee, on affiche dans le terminal le score final
+    print("Final score = " + str(score.score()))
+
+    if pygame.mixer:
+        pygame.mixer.music.fadeout(1000)
+    pygame.time.wait(1000)
+    pygame.quit()
+
+
+# appelle la fonction main si on execute le script
+if __name__ == '__main__':
+    main()
+
